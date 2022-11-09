d6a56a60e503b1e0a05245e37dae6c8d6c1acce6
Lucía <lucialarraona98@gmail.com>
2022-11-09 13:54:15 +0100

    fixed csv with correct data formats

--- Notebooks/helper_functions.py
+++ Notebooks/helper_functions.py
@@ -2,6 +2,23 @@
 import bz2
 import pickle
 import _pickle as cPickle
+import ast
+
+
+
+
+
+# Convert list stored as string to actual string object 
+def convert_type(string):
+    a_list = ast.literal_eval(string) 
+    return a_list
+
+
+# Pickle a file and then compress it into a file with extension 
+def compressed_pickle(title, data):
+ with bz2.BZ2File(title + '.pbz2', 'w') as f: 
+    cPickle.dump(data, f)
+
 
 # Load any compressed pickle file
 def decompress_pickle(file):
