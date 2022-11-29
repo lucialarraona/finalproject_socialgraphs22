80fd0f5d41d65d935925591d268ed33a06d9e230
Lucía <lucialarraona98@gmail.com>
2022-11-29 22:12:15 +0100

    update 29nov nlp

--- Notebooks/helper_functions.py
+++ Notebooks/helper_functions.py
@@ -25,3 +25,58 @@ def decompress_pickle(file):
  data = bz2.BZ2File(file, 'rb')
  data = cPickle.load(data)
  return data
+
+
+def plot_pretty_hist(list_data, percentiles, mean, plot_perct = False, title = str, color=str):
+
+    x = [val for (node, val) in list_data]
+    fig,ax = plt.subplots(figsize = (10,6))
+    cnts, values, bars = ax.hist(x, edgecolor='black', 
+                                        bins=10,
+                                        color=color,
+                                        alpha = 0.2)
+
+    if plot_perct == True:
+        # Plot mean
+        ax.axvline(mean, color='black', linestyle = '-', linewidth=4) 
+        # Plot percentiles and mean lines
+        quants = [[percentiles[0], 0.8, 0.90], [percentiles[1], 0.8, 0.40]]
+        for i in quants:
+            ax.axvline(i[0], alpha = i[1], ymax = i[2], linestyle = ":", color = 'Red' ,linewidth=3)  
+
+        
+        # Labels and titles
+        ax.set_ylabel("Page Count")
+        ax.set_xlabel("Average Sentiment")
+        ax.set_title(f"{title}")
+        ax.legend(['Mean','90th percentile','10th percentile'])
+        caption = ("Figure: Histogram of the mean average of every character page for the Marvel universe.\n"
+                    "Black line represents the mean across pages and the red dotted lines the 10th and 90th percentile respectively.")
+        fig.text(0.02,-0.1, caption, ha='left')
+        
+        # Remove ticks and spines
+        for ax, spine in ax.spines.items():
+            spine.set_visible(True)
+        plt.show()
+        
+        
+
+    else:
+        
+        # Plot the mean
+        ax.axvline(mean, color='black', linewidth=4) 
+        
+        # Labels and titles
+        ax.set_ylabel("Page Count")
+        ax.set_xlabel("Average Sentiment")
+        ax.set_title(f"{title}")
+        ax.legend(['Mean'])
+        caption = ('haha')
+        fig.text(0.02,-0.1, caption, ha='left')
+        
+        # Remove ticks and spines
+        for ax, spine in ax.spines.items():
+            spine.set_visible(True)
+        plt.show()
+
+    return None
